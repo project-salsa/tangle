42e267594894377b67be3a1a39072e2360276bf3
Reno DuBois <rdubois96@gmail.com>
2017-09-04 15:50:20 -0500

    Added a Test component to show off React Native

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Test from './components/test';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>
+          Here is some text
+        </Text>
+        <Test />
       </View>
     );
   }
