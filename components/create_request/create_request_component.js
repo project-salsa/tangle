bbe39b5b3d64393fef815fd9aec599f0d7a31c81
jpadgett7 <johnpadgett7@gmail.com>
2017-09-07 17:01:20 -0500

    Added Picker component for selecting a game to Create Request Page

--- components/create_request/create_request_component.js
+++ components/create_request/create_request_component.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Container, Header, Body, Title, Form, Text, Left, Center, Content, Picker, Button} from "native-base";
+
 
 export default class CreateRequestComponent extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>Change and replace me!</Text>
-      </View>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: undefined
+        };
+    }
+
+    onValueChange(value: string) {
+        this.setState({
+           selected: value
+        });
+    }
+
+    render() {
+        return(
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent onPress={() =>this.props.navigation.goBack()}>
+                            <Icon name="back-arrow" />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Create Game Request</Title>
+                    </Body>
+                </Header>
+                <Content>
+                    <Body>
+                        <Form>
+                            <Picker
+                                mode="dropdown"
+                                placeholder="Select a Game"
+                                selectedValue={this.state.selected}
+                                onValueChange={this.onValueChange.bind(this)}
+                            >
+                                <Item label="League of Legends" value="lol" />
+                                <Item label="Overwatch" value="ow" />
+                                <Item label="Super Smash Bros. Melee" value="melee" />
+                                <Item label="Super Smash Bros. for Wii U" value="smash4" />
+                            </Picker>
+                        </Form>
+                    </Body>
+                </Content>
+            </Container>
+        );
+    }
 }
