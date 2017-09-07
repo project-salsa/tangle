e8724c5548f42bff5fd165d6ee3fd23878891716
jpadgett7 <johnpadgett7@gmail.com>
2017-09-07 17:53:22 -0500

    Fixed issue with <Title> component causing crash in Expo

--- components/create_request/create_request_component.js
+++ components/create_request/create_request_component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Header, Body, Title, Form, Text, Left, Center, Content, Picker, Button} from "native-base";
+import {Container, Header, Body, Title, Text, Form, Left, Right, Content, Picker, Button, Icon, Item} from "native-base";
 
 
 export default class CreateRequestComponent extends React.Component {
@@ -22,29 +22,27 @@ export default class CreateRequestComponent extends React.Component {
                 <Header>
                     <Left>
                         <Button transparent onPress={() =>this.props.navigation.goBack()}>
-                            <Icon name="back-arrow" />
+                            <Icon name="arrow-back" />
                         </Button>
                     </Left>
                     <Body>
-                        <Title>Create Game Request</Title>
+                        <Text>Create Game Request</Text>
                     </Body>
                 </Header>
                 <Content>
-                    <Body>
-                        <Form>
-                            <Picker
-                                mode="dropdown"
-                                placeholder="Select a Game"
-                                selectedValue={this.state.selected}
-                                onValueChange={this.onValueChange.bind(this)}
-                            >
-                                <Item label="League of Legends" value="lol" />
-                                <Item label="Overwatch" value="ow" />
-                                <Item label="Super Smash Bros. Melee" value="melee" />
-                                <Item label="Super Smash Bros. for Wii U" value="smash4" />
-                            </Picker>
-                        </Form>
-                    </Body>
+                    <Form>
+                        <Picker
+                            mode="dropdown"
+                            placeholder="Select a Game"
+                            selectedValue={this.state.selected}
+                            onValueChange={this.onValueChange.bind(this)}
+                        >
+                            <Item label="League of Legends" value="game0" />
+                            <Item label="Overwatch" value="game1" />
+                            <Item label="Super Smash Bros. Melee" value="game2" />
+                            <Item label="Super Smash Bros. for Wii U" value="game3" />
+                        </Picker>
+                    </Form>
                 </Content>
             </Container>
         );
