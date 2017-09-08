6e845ff514a1860c49e82dad186a50d04a7e8c0b
DanFuchs96 <DanFuchs96@gmail.com>
2017-09-08 00:07:28 -0500

    Type mismatch fixes

--- components/first_time/first_time_component.js
+++ components/first_time/first_time_component.js
@@ -12,27 +12,27 @@ export default class FirstTimeComponent extends React.Component {
               <View style={{height:15}}/>
               <Text style={{fontSize: 30, fontWeight: 'bold'}}>Welcome!</Text>
               <View style={{height:15}}/>
-              <Text style={{justify:'right'}}>Insert a general summary of the app and what it does in here.</Text>
+              <Text style={{textAlign: 'justify'}}>Insert a general summary of the app and what it does in here.</Text>
               <View style={{height:25}}/>
               <Text>Enter your data value!</Text>
               <TextInput
                   style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
                   placeholder="Field Placeholder"
-                  selectTextOnFocus="true"
+                  selectTextOnFocus=true
               />
               <View style ={{height:25}} />
               <Text>Enter your data value!</Text>
               <TextInput
                   style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
                   placeholder="Field Placeholder"
-                  selectTextOnFocus="true"
+                  selectTextOnFocus=true
               />
               <View style ={{height:25}} />
               <Text>Enter your data value!</Text>
               <TextInput
                   style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
                   placeholder="Field Placeholder"
-                  selectTextOnFocus="true"
+                  selectTextOnFocus=true
               />
               <Button
                   onPress={() => alert("Ideally, I'd submit information. Probably.")}
