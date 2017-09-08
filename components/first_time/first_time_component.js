9b4e2d5bfc8fe353977cf25cdb2776ceb677d616
DanFuchs96 <DanFuchs96@gmail.com>
2017-09-07 23:53:58 -0500

    Generic Framework Created

--- components/first_time/first_time_component.js
+++ components/first_time/first_time_component.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, AppRegistry, TextInput, Button } from 'react-native';
 
 export default class FirstTimeComponent extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>I'm the first time page!</Text>
-      </View>
-    )
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={{flex:1, backgroundColor: 'beige', alignItems: 'center'}}>
+              <View style={{height:15}}/>
+              <Text style={{fontSize: 30, fontWeight: 'bold'}}>Welcome!</Text>
+              <View style={{height:15}}/>
+              <Text style={{justify:'right'}}>Insert a general summary of the app and what it does in here.</Text>
+              <View style={{height:25}}/>
+              <Text>Enter your data value!</Text>
+              <TextInput
+                  style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
+                  placeholder="Field Placeholder"
+                  selectTextOnFocus="true"
+              />
+              <View style ={{height:25}} />
+              <Text>Enter your data value!</Text>
+              <TextInput
+                  style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
+                  placeholder="Field Placeholder"
+                  selectTextOnFocus="true"
+              />
+              <View style ={{height:25}} />
+              <Text>Enter your data value!</Text>
+              <TextInput
+                  style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
+                  placeholder="Field Placeholder"
+                  selectTextOnFocus="true"
+              />
+              <Button
+                  onPress={() => alert("Ideally, I'd submit information. Probably.")}
+                  title="Next"
+              />
+            </View>
+        );
+    }
 }
