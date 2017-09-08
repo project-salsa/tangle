7404a2a95272fbb4077a622a8468fa89ed7a5732
DanFuchs96 <DanFuchs96@gmail.com>
2017-09-08 01:29:43 -0500

    Added visual scaling
    The prompts and whatnot scale nicely to the screen's dimensions now. You can even turn the phone without a loss in spacing between items.

--- components/first_time/first_time_component.js
+++ components/first_time/first_time_component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, AppRegistry, TextInput, Button } from 'react-native';
+import { Text, View, AppRegistry, TextInput, Button, ScrollView } from 'react-native';
 
 export default class FirstTimeComponent extends React.Component {
     constructor(props) {
@@ -8,37 +8,76 @@ export default class FirstTimeComponent extends React.Component {
 
     render() {
         return (
-            <View style={{flex:1, backgroundColor: 'beige', alignItems: 'center'}}>
-              <View style={{height:15}}/>
-              <Text style={{fontSize: 30, fontWeight: 'bold'}}>Welcome!</Text>
-              <View style={{height:15}}/>
-              <Text style={{textAlign: 'justify'}}>Insert a general summary of the app and what it does in here.</Text>
-              <View style={{height:25}}/>
-              <Text>Enter your data value!</Text>
-              <TextInput
-                  style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
-                  placeholder="Field Placeholder"
-                  selectTextOnFocus={true}
-              />
-              <View style ={{height:25}} />
-              <Text>Enter your data value!</Text>
-              <TextInput
-                  style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
-                  placeholder="Field Placeholder"
-                  selectTextOnFocus={true}
-              />
-              <View style ={{height:25}} />
-              <Text>Enter your data value!</Text>
-              <TextInput
-                  style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
-                  placeholder="Field Placeholder"
-                  selectTextOnFocus={true}
-              />
-              <Button
-                  onPress={() => alert("Ideally, I'd submit information. Probably.")}
-                  title="Next"
-              />
-            </View>
+            <ScrollView
+                pagingEnabled={true}
+            >
+              <View style={{height: 700, flexDirection:'row'}}>
+                <View style={{flex:1, backgroundColor: 'beige'}}/>
+                <View style={{flex:10, flexDirection:'column', backgroundColor: 'beige', alignItems: 'center'}}>
+                  <View style={{flex:3}}/>
+                  <Text style={{fontSize: 30, fontWeight: 'bold'}}>Welcome!</Text>
+                  <View style={{flex:3}}/>
+                  <Text style={{textAlign: 'justify'}}>
+                    Insert a general summary of the app and what it does in here.
+                    So much text here.
+                    So many lines.
+                  </Text>
+                  <View style={{flex:5}}/>
+                  <Text>Enter your data value!</Text>
+                  <View style={{flex:5, flexDirection:'row'}}>
+                    <TextInput
+                        style={{backgroundColor: 'white', flex:1, height:40, borderColor: 'gray', borderWidth: 1}}
+                        placeholder="Field Placeholder"
+                        selectTextOnFocus={true}
+                    />
+                  </View>
+                  <View style={{flex:5}} />
+                  <Text>Enter your data value!</Text>
+                  <View style={{flex:5, flexDirection:'row'}}>
+                    <TextInput
+                        style={{backgroundColor: 'white', flex:1, height:40, borderColor: 'gray', borderWidth: 1}}
+                        placeholder="Field Placeholder"
+                        selectTextOnFocus={true}
+                    />
+                  </View>
+                  <View style={{flex:5}} />
+                  <Text>Enter your data value!</Text>
+                  <View style={{flex:5, flexDirection:'row'}}>
+                    <TextInput
+                        style={{backgroundColor: 'white', flex:1, height:40, borderColor: 'gray', borderWidth: 1}}
+                        placeholder="Field Placeholder"
+                        selectTextOnFocus={true}
+                    />
+                  </View>
+                  <View style={{flex:5}} />
+                  <Text>Enter your data value!</Text>
+                  <View style={{flex:5, flexDirection:'row'}}>
+                    <TextInput
+                        style={{backgroundColor: 'white', flex:1, height:40, borderColor: 'gray', borderWidth: 1}}
+                        placeholder="Field Placeholder"
+                        selectTextOnFocus={true}
+                    />
+                  </View>
+                  <View style={{flex:5}} />
+                  <Text>Enter your data value!</Text>
+                  <View style={{flex:5, flexDirection:'row'}}>
+                    <TextInput
+                        style={{backgroundColor: 'white', flex:1, height:40, borderColor: 'gray', borderWidth: 1}}
+                        placeholder="Field Placeholder"
+                        selectTextOnFocus={true}
+                    />
+                  </View>
+                  <View style={{flex:3}} />
+                  <Button
+                      color="red"
+                      onPress={() => alert("Ideally, I'd submit information. Probably.")}
+                      title="Next"
+                  />
+                  <View style={{flex:10}} />
+                </View>
+                <View style={{flex:1, backgroundColor: 'beige'}}/>
+              </View>
+            </ScrollView>
         );
     }
 }
