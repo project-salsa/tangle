e6e98ed8fc5b9f71f1595de009cbbc9ac6ad35c4
DanFuchs96 <DanFuchs96@gmail.com>
2017-09-08 00:15:58 -0500

    Fixed type mismatches

--- components/first_time/first_time_component.js
+++ components/first_time/first_time_component.js
@@ -18,21 +18,21 @@ export default class FirstTimeComponent extends React.Component {
               <TextInput
                   style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
                   placeholder="Field Placeholder"
-                  selectTextOnFocus=true
+                  selectTextOnFocus={true}
               />
               <View style ={{height:25}} />
               <Text>Enter your data value!</Text>
               <TextInput
                   style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
                   placeholder="Field Placeholder"
-                  selectTextOnFocus=true
+                  selectTextOnFocus={true}
               />
               <View style ={{height:25}} />
               <Text>Enter your data value!</Text>
               <TextInput
                   style={{backgroundColor: 'white', height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
                   placeholder="Field Placeholder"
-                  selectTextOnFocus=true
+                  selectTextOnFocus={true}
               />
               <Button
                   onPress={() => alert("Ideally, I'd submit information. Probably.")}
