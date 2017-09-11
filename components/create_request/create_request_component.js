3ce20b01bd6c525365198fec033186871a236fc0
jpadgett7 <johnpadgett7@gmail.com>
2017-09-11 13:24:53 -0500

    Player Count field added, input data now stores locally

--- components/create_request/create_request_component.js
+++ components/create_request/create_request_component.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {Container, Header, Body, Title, Text, Form, Left, Right, Content, Picker, Button, Icon, Item, Input, Label} from "native-base";
-
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
 export default class CreateRequestComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: undefined
+            gameSelection: undefined,
+            playerCount: undefined,
+            locationName: undefined
         };
     }
 
     onValueChange(value: string) {
-        this.setState({
-           selected: value
-        });
+        if (value !== "none") {
+            this.setState({
+                gameSelection: value
+            });
+        }
     }
 
     render() {
@@ -33,30 +37,40 @@ export default class CreateRequestComponent extends React.Component {
                 </Header>
                 <Content padder>
                     <Form>
+                        <Text>Select a Game</Text>
                         <Picker
-                            mode="dropdown"
-                            placeholder="Select a Game"
-                            selectedValue={this.state.selected}
+                            iosHeader="Select a Game"
+                            mode="dialog"
+                            prompt="Select a Game"
+                            selectedValue={this.state.gameSelection}
                             onValueChange={this.onValueChange.bind(this)}
                         >
+                            <Item label="No Game Selected" value="none" />
                             <Item label="League of Legends" value="game0" />
                             <Item label="Overwatch" value="game1" />
                             <Item label="Super Smash Bros. Melee" value="game2" />
                             <Item label="Super Smash Bros. for Wii U" value="game3" />
                         </Picker>
                         <Item floatingLabel>
+                            <Icon active ios='ios-happy' android='md-happy' />
+                            <Label>Number of Players</Label>
+                            <Input
+                                onChangeText={(input) => this.setState({playerCount:input})}
+                            />
+                        </Item>
+                        <Item floatingLabel>
                             <Icon active ios='ios-pin' android='md-pin' />
                             <Label>Location</Label>
-                            <Input />
+                            <Input
+                                onChangeText={(input) => this.setState({locationName:input})}
+                            />
                         </Item>
-                        <Body>
-                            <Button full primary
-                                onPress={() => navigate('Dashboard')}
-                            >
-                                <Text>Send Request</Text>
-                            </Button>
-                        </Body>
                     </Form>
+                    <Button full primary
+                            onPress={() => navigate('Dashboard')}
+                    >
+                        <Text>Send Request</Text>
+                    </Button>
                 </Content>
             </Container>
         );
