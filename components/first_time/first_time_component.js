31d663ed315faff8f9ead1be854d09845a6481b7
DanFuchs96 <DanFuchs96@gmail.com>
2017-09-10 21:54:22 -0500

    Improving Formatting

--- components/first_time/first_time_component.js
+++ components/first_time/first_time_component.js
@@ -9,73 +9,73 @@ export default class FirstTimeComponent extends React.Component {
     render() {
         return (
             <ScrollView pagingEnabled={true}>
-                <View style={{height: 700, flexDirection:'row'}}>
-                    <View style={{flex:1, backgroundColor: 'beige'}}/>
-                    <View style={{flex:10, flexDirection:'column', backgroundColor: 'beige', alignItems: 'center'}}>
+                <View style={{height: 700, flexDirection:"row"}}>
+                    <View style={{flex:1, backgroundColor: "beige"}}/>
+                    <View style={{flex:10, flexDirection:"column", backgroundColor: "beige", alignItems: "center"}}>
                         <View style={{flex:3}}/>
-                        <Text style={{fontSize: 30, fontWeight: 'bold'}}>Welcome!</Text>
+                        <Text style={{fontSize: 32, fontWeight: "bold"}}>Welcome!</Text>
                         <View style={{flex:3}}/>
-                        <Text style={{textAlign: 'justify'}}>
+                        <Text style={{textAlign: "justify"}}>
                             Insert a general summary of the app and what it does in here.
-                            So much text here.
-                            So many lines.
+                            Ideally, we would have multiple of these paragraphs, prompting different sections of inputs.
+                            For now, this paragraph will serve as an example header.
                         </Text>
                         <View style={{flex:5}}/>
                         <Text>Enter your data value!</Text>
-                        <View style={{flex:5, flexDirection:'row'}}>
+                        <View style={{flex:5, flexDirection:"row"}}>
                             <TextInput
                                 style={styles.genericTextBox}
-                                placeholder="Field Placeholder"
+                                placeholder="  Field Placeholder"
                                 selectTextOnFocus={true}
                             />
                         </View>
                         <View style={{flex:5}} />
                         <Text>Enter your data value!</Text>
-                        <View style={{flex:5, flexDirection:'row'}}>
+                        <View style={{flex:5, flexDirection:"row"}}>
                             <TextInput
                                 style={styles.genericTextBox}
-                                placeholder="Field Placeholder"
+                                placeholder="  Field Placeholder"
                                 selectTextOnFocus={true}
                             />
                         </View>
                         <View style={{flex:5}} />
                         <Text>Enter your data value!</Text>
-                        <View style={{flex:5, flexDirection:'row'}}>
+                        <View style={{flex:5, flexDirection:"row"}}>
                             <TextInput
                                 style={styles.genericTextBox}
-                                placeholder="Field Placeholder"
+                                placeholder="  Field Placeholder"
                                 selectTextOnFocus={true}
                             />
                         </View>
                         <View style={{flex:5}} />
                         <Text>Enter your data value!</Text>
-                        <View style={{flex:5, flexDirection:'row'}}>
+                        <View style={{flex:5, flexDirection:"row"}}>
                             <TextInput
                                 style={styles.genericTextBox}
-                                placeholder="Field Placeholder"
+                                placeholder="  Field Placeholder"
                                 selectTextOnFocus={true}
                             />
                         </View>
                         <View style={{flex:5}} />
                         <Text>Enter your data value!</Text>
-                        <View style={{flex:5, flexDirection:'row'}}>
+                        <View style={{flex:5, flexDirection:"row"}}>
                             <TextInput
                                 style={styles.genericTextBox}
-                                placeholder="Field Placeholder"
+                                placeholder="  Field Placeholder"
                                 selectTextOnFocus={true}
                             />
                         </View>
                         <View style={{flex:7}} />
-                        <View style={{flex:7, flexDirection: 'row'}}>
+                        <View style={{flex:7, flexDirection: "row"}}>
                             <TouchableOpacity
                                 style={styles.contButton}
                                 onPress={() => alert("Ideally, this would do something.")}>
-                                <Text style={{fontSize: 24, color:"white", fontWeight:"bold"}}>CONTINUE</Text>
+                                <Text style={styles.contButtonText}>CONTINUE</Text>
                             </TouchableOpacity>
                         </View>
                         <View style={{flex:7}} />
                     </View>
-                    <View style={{flex:1, backgroundColor: 'beige'}}/>
+                    <View style={{flex:1, backgroundColor: "beige"}}/>
                 </View>
             </ScrollView>
         );
@@ -84,17 +84,23 @@ export default class FirstTimeComponent extends React.Component {
 
 const styles = StyleSheet.create({
     genericTextBox: {
-        backgroundColor: 'white',
+        backgroundColor: "white",
         flex: 1,
         height: 40,
-        borderColor: 'gray',
-        borderWidth: 1
+        borderColor: "gray",
+        borderWidth: 2,
+        fontStyle: "italic"
     },
     contButton: {
-        backgroundColor: 'blue',
+        backgroundColor: "blue",
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    contButtonText: {
+        fontSize: 24,
+        color:"white",
+        fontWeight:"bold"
     }
 });
