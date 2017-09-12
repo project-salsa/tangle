16949d6fb32a0bb53beb27e428b2810b4c6839f7
Reno DuBois <rdubois96@gmail.com>
2017-09-12 01:52:36 -0500

    Added Standard code style, removed test.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import HomeComponent from './components/home_screen';
-import DashboardComponent from './components/dashboard/dashboard_component';
-import RequestComponent from "./components/request/request_component";
-import UserProfileComponent from "./components/user_profile/user_profile_component";
-import LoginComponent from "./components/login/login_component";
-import UserPreferencesComponent from "./components/user_preferences/user_preferences_component";
+import { AppRegistry } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+import HomeComponent from './components/home_screen'
+import DashboardComponent from './components/dashboard/dashboard_component'
+import RequestComponent from './components/request/request_component'
+import UserProfileComponent from './components/user_profile/user_profile_component'
+import LoginComponent from './components/login/login_component'
+import UserPreferencesComponent from './components/user_preferences/user_preferences_component'
 
 const App = StackNavigator({
   Home: { screen: HomeComponent },
@@ -14,9 +13,9 @@ const App = StackNavigator({
   Request: { screen: RequestComponent },
   UserProfile: { screen: UserProfileComponent },
   Login: { screen: LoginComponent },
-  UserPreferences: { screen: UserPreferencesComponent },
-});
+  UserPreferences: { screen: UserPreferencesComponent }
+})
 
-export default App;
+export default App
 
-AppRegistry.registerComponent('App', () => App);
+AppRegistry.registerComponent('App', () => App)
