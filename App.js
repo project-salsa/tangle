6e519783a29d5093a72c7fd9e10a789ba2ca1bca
Reno DuBois <rdubois96@gmail.com>
2017-09-12 01:22:32 -0500

    Added StandardJs code styling to project

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import HomeComponent from './components/home_screen';
-import DashboardComponent from './components/dashboard/dashboard_component';
-import RequestComponent from "./components/request/request_component";
-import UserProfileComponent from "./components/user_profile/user_profile_component";
-import LoginComponent from "./components/login/login_component";
-import UserPreferencesComponent from "./components/user_preferences/user_preferences_component";
-import CreateRequestComponent from "./components/create_request/create_request_component";
+import { AppRegistry } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+import HomeComponent from './components/home_screen'
+import DashboardComponent from './components/dashboard/dashboard_component'
+import RequestComponent from './components/request/request_component'
+import UserProfileComponent from './components/user_profile/user_profile_component'
+import LoginComponent from './components/login/login_component'
+import UserPreferencesComponent from './components/user_preferences/user_preferences_component'
+import CreateRequestComponent from './components/create_request/create_request_component'
+import RequestCard from './components/common/request_card'
 
 const App = StackNavigator({
   Home: { screen: HomeComponent },
@@ -16,9 +16,10 @@ const App = StackNavigator({
   UserProfile: { screen: UserProfileComponent },
   Login: { screen: LoginComponent },
   UserPreferences: { screen: UserPreferencesComponent },
-  CreateRequest: { screen: CreateRequestComponent }
-});
+  CreateRequest: { screen: CreateRequestComponent },
+  RequestCard: { screen: RequestCard }
+})
 
-export default App;
+export default App
 
-AppRegistry.registerComponent('App', () => App);
+AppRegistry.registerComponent('App', () => App)
--- components/home_screen.js
+++ components/home_screen.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React from 'react'
+import {View, Text, Button} from 'react-native'
 
 export default class HomeComponent extends React.Component {
-  render() {
-    const { navigate } = this.props.navigation;
+  render () {
+    const { navigate } = this.props.navigation
 
     return (
       <View>
         <Text>This screen is for development: it will not be included in the main app at all.</Text>
         <Button
-          onPress={() => navigate('Dashboard')}
-          title="Go to Dashboard"
+          onPress={() => navigate('Dashboard', {navigation: this.props.navigation})}
+          title='Go to Dashboard'
         />
         <Button
           onPress={() => navigate('Request')}
-          title="Go to Request"
+          title='Go to Request'
         />
         <Button
           onPress={() => navigate('UserProfile')}
-          title="Go to User Profile"
+          title='Go to User Profile'
         />
         <Button
           onPress={() => navigate('Login')}
-          title="Go to Login"
+          title='Go to Login'
         />
         <Button
           onPress={() => navigate('UserPreferences')}
-          title="Go to User Prefs."
+          title='Go to User Prefs.'
         />
         <Button
           onPress={() => navigate('CreateRequest')}
-          title="Go to Create Request"
+          title='Go to Create Request'
         />
       </View>
     )
