a1bc3642c528b286c669b8c43f3ddac10ba64a54
DanFuchs96 <DanFuchs96@gmail.com>
2017-09-16 22:09:39 -0500

    Basic Login Page

--- components/login/login_component.js
+++ components/login/login_component.js
@@ -1,12 +1,55 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {Container, Header, Body, Title, Text, Form, Left, Right, Content, View, Button, Icon, Item, Input, Label} from 'native-base'
 
-export default class LoginComponent extends React.Component {
-  render () {
-    return (
-      <View>
-        <Text>Change and replace me!</Text>
-      </View>
-    )
-  }
+export default class CreateRequestComponent extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            username: undefined,
+            password: undefined
+        }
+    }
+
+    render () {
+        const { navigate } = this.props.navigation;
+        return (
+            <Container>
+              <Header style={{backgroundColor: "black"}}>
+                <Body>
+                <Title style={{fontWeight: "bold", fontSize: 30}}>Welcome to Tangle!</Title>
+                </Body>
+              </Header>
+              <Content padder>
+                <Form style={{backgroundColor:"#AAAAAA"}}>
+                  <Item floatingLabel>
+                    <Icon active name="bulb" />
+                    <Label>  Enter Username</Label>
+                    <Input onChangeText={(input) => this.setState({username: input})} />
+                  </Item>
+                  <Item floatingLabel>
+                    <Icon active name="lock" />
+                    <Label>  Enter Password</Label>
+                    <Input secureTextEntry={true} onChangeText={(input) => this.setState({password: input})} />
+                  </Item>
+                </Form>
+                <View style={{backgroundColor: "#999999", height: 15}}/>
+                <View style={{height: 10}}/>
+                <View>
+                  <Button full primary
+                          onPress={() => {
+                            alert("Inputs: " + this.state.username + ", " + this.state.password);
+                            navigate('Dashboard');
+                          }}
+                  >
+                    <Text>Login</Text>
+                  </Button>
+                  <View style={{height: 15}}/>
+                  <Button full danger onPress={() => navigate('UserProfile')}>
+                    <Text>Need to Register?</Text>
+                  </Button>
+                </View>
+              </Content>
+            </Container>
+        )
+    }
 }
