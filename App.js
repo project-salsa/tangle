c33280d28da4a3dba048d9055de1365e627de070
Reno DuBois <renodubois@users.noreply.github.com>
2017-11-02 16:23:13 -0500

    Adds MobX + auth store

--- App.js
+++ App.js
@@ -1,27 +1,18 @@
+import React from 'react'
 import { AppRegistry } from 'react-native'
-import { StackNavigator } from 'react-navigation'
-import HomeComponent from './components/home_screen'
-import UserProfileContainer from './containers/user_profile/user_profile_container'
-import DashboardContainer from './containers/dashboard_container'
-import RequestContainer from './containers/request/request_container'
-import LoginComponent from './components/login/login_component'
-import UserPreferencesComponent from './components/user_preferences/user_preferences_component'
-import CreateRequestContainer from './containers/create_request/create_request_container'
-import RequestCard from './components/common/request_card'
-import FirstTimeComponent from './components/first_time/first_time_component'
+import { Provider } from 'mobx-react'
 
-const App = StackNavigator({
-  Home: { screen: HomeComponent },
-  UserProfile: { screen: UserProfileContainer },
-  Dashboard: { screen: DashboardContainer },
-  Request: { screen: RequestContainer },
-  Login: { screen: LoginComponent },
-  UserPreferences: { screen: UserPreferencesComponent },
-  CreateRequest: { screen: CreateRequestContainer },
-  FirstTime: { screen: FirstTimeComponent },
-  RequestCard: { screen: RequestCard }
-})
+import stores from './stores'
+import Router from './router'
 
-export default App
+class App extends React.Component {
+  render () {
+    return (
+      <Provider {...stores}>
+        <Router />
+      </Provider>
+    )
+  }
+}
 
 AppRegistry.registerComponent('App', () => App)
---
+++ router.js
@@ -0,0 +1,26 @@
+import { StackNavigator } from 'react-navigation'
+import HomeComponent from './components/home_screen'
+import UserProfileContainer from './containers/user_profile/user_profile_container'
+import DashboardContainer from './containers/dashboard_container'
+import RequestContainer from './containers/request/request_container'
+import LoginComponent from './components/login/login_component'
+import UserPreferencesComponent from './components/user_preferences/user_preferences_component'
+import CreateRequestContainer from './containers/create_request/create_request_container'
+import RequestCard from './components/common/request_card'
+import FirstTimeComponent from './components/first_time/first_time_component'
+
+const navOptions = {
+  initialRouteName: 'Home'
+}
+
+export default StackNavigator({
+  Home: { screen: HomeComponent },
+  UserProfile: { screen: UserProfileContainer },
+  Dashboard: { screen: DashboardContainer },
+  Request: { screen: RequestContainer },
+  Login: { screen: LoginComponent },
+  UserPreferences: { screen: UserPreferencesComponent },
+  CreateRequest: { screen: CreateRequestContainer },
+  FirstTime: { screen: FirstTimeComponent },
+  RequestCard: { screen: RequestCard }
+}, navOptions)
