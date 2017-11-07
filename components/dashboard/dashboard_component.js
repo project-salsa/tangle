c38c1b3873fe0510f24112a22d202f9012d756bb
Reno DuBois <renodubois@users.noreply.github.com>
2017-11-07 14:42:39 -0600

    Make login request to backend

--- components/dashboard/dashboard_component.js
+++ components/dashboard/dashboard_component.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Container, Content, H1 } from 'native-base'
 import RequestList from './request_list'
+import { inject } from 'mobx-react'
 
+@inject('authStore')
 export default class DashboardComponent extends React.Component {
   render () {
+    console.log('The token! ', this.props.authStore.token)
+
     return (
       <Container>
         <Content>
