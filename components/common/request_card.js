d2533bed9f716b3527b88707c8eaafe5083dbd4c
Reno DuBois <renodubois@users.noreply.github.com>
2017-12-02 20:02:42 -0600

    More ddashboard beautification

--- components/common/request_card.js
+++ components/common/request_card.js
@@ -1,25 +1,37 @@
 import React from 'react'
-import { Card, CardItem, Left, Text } from 'native-base'
+import { Card, CardItem, Icon, View, Text } from 'native-base'
 
 export default class RequestCard extends React.Component {
   render () {
     const { navigate } = this.props.navigation
     return (
       <Card>
-        <CardItem button style={{backgroundColor: '#BBAFBA'}}
+        <CardItem button style={{backgroundColor: '#EEEEEE'}}
                   onPress={() => navigate('Request', { requestId: this.props.requestId })}>
-            <Left>
-            <Text>
-              {this.props.title}
-              {'\n'}
-              {this.props.user}
-            </Text>
-          </Left>
-          <Text>
-            {this.props.game}
-            {'\n'}
-            {this.props.location}
-          </Text>
+          <View style={{flex:1, backgroundColor: '#887C87'}}>
+            <View style={{flex: 1}} />
+            <View style={{flex: 20, flexDirection: 'row'}}>
+              <View style={{flex: 2}}>
+                <View style={{height: 7}} />
+                <Icon active name='man' style={{padding: 10}} />
+              </View>
+              <View style={{flex: 20, flexDirection: 'row'}}>
+                <View style={{flex: 3}}>
+                  <Text style={{padding: 1}}>
+                    {this.props.user}
+                  </Text>
+                </View>
+                <View style={{flex: 8}}>
+                  <Text style={{padding: 1}}>
+                    {this.props.game} {'\n'}
+                    "{this.props.title}"
+                  </Text>
+                </View>
+              </View>
+              <View style={{flex: 1}} />
+            </View>
+            <View style={{flex: 1}} />
+          </View>
         </CardItem>
       </Card>
     )
@@ -32,5 +44,5 @@ RequestCard.defaultProps = {
   user: '', // maybe this should be userId and we can grab info?
   tags: [], // may need to be changed based on what we want to do w/ the tag system
   game: '', // probably just a temporary prop
-  location: '' // issa string now but this should be some sort of location data type
+  location: ''
 }
