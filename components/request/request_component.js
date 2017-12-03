2f83a4dcc3f09d4e8e7c20c5b4c08adc3f6139b3
Reno DuBois <renodubois@users.noreply.github.com>
2017-12-03 00:37:36 -0600

    Fix merge issues

--- components/request/request_component.js
+++ components/request/request_component.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Text, View, Image} from 'react-native'
-import {Container, Header, Body, Title, Left, Content, Button, Icon, Thumbnail} from 'native-base'
+import {Container, Body, Title, Left, Content, Button, Icon, Thumbnail} from 'native-base'
 import { Col, Row, Grid } from 'react-native-easy-grid'
 import { inject } from 'mobx-react'
 import axios from 'axios'
